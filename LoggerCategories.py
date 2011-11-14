9447529aaaba8c323394c5f117d5e46fadc6b052
James Perkins <jperkins@redhat.com>
2011-11-14 08:21:35 -0800

    Add script to print logger categories and fix the message checker.

--- LoggerCategories.py
+++ LoggerCategories.py
@@ -26,48 +26,54 @@ import argparse
 from os import path, listdir
 from re import search
 
-def processFile(file):
+
+def processFile(file, printFileName):
     current = open(file)
     lines = []
     for line in current:
         trimmedLine = line.strip()
-        if trimmedLine.startswith("@MessageBundle") or trimmedLine.startswith("@MessageLogger"):
-            return
-            # Skip comments
+        # Skip comments
         if trimmedLine.startswith("//") == False and trimmedLine.startswith("*") == False:
-            # Check for exceptions being thrown.
-            if trimmedLine.find("throw new") != -1 and search('"[^"\\\r\n]*(?:\\.[^"\\\r\n]*)*"', trimmedLine) != None:
-                lines.append(trimmedLine)
-            # Check for text that has at least one space.
-            elif search('"[^"\\\r\n]*\w\s.*(?:\\.[^"\\\r\n]*)*"', trimmedLine) != None:
-                lines.append(trimmedLine)
-        # Check to see if lines were found and print
+            if trimmedLine.find("Logger.getLogger") > -1:
+                m = search('"[^"\\\r\n]*(?:\\.[^"\\\r\n]*)*"', trimmedLine)
+                if m != None:
+                    lines.append(m.group(0))
+                m = search('\([^"\\\r\n]*(?:\\.[^"\\\r\n]*)*\.class\)', trimmedLine)
+                if m != None:
+                    lines.append(m.group(0))
+    # Check to see if lines were found and print
     if len(lines) > 0:
-        print(str(file) + ":")
-        for line in lines:
-            print("    " + line)
-            print("")
+        if printFileName:
+            print(str(file) + ":")
+            for line in lines:
+                print("    " + line)
+                print("")
+        else:
+            for line in lines:
+                print(line)
 
 
-def processDir(dir):
+def processDir(dir, printFileName):
     files = listdir(dir)
     for file in files:
         file_path = path.join(dir, file)
         if path.isfile(file_path) & file_path.endswith(".java"):
-            processFile(file_path)
+            processFile(file_path, printFileName)
         elif path.isdir(file_path):
-            processDir(file_path)
+            processDir(file_path, printFileName)
 
 # Parse the incoming parameters
-parser = argparse.ArgumentParser(description="Check messages")
-parser.add_argument("-f", "--file", metavar="FILE", type=str, dest="filename",
-                    help="The file or directory to scan messages for.")
+parser = argparse.ArgumentParser(description="List Logging Categories")
+parser.add_argument("-f", "--file", metavar="FILE", type=str, dest="filename", help="The file or directory to scan for logging categories.")
+parser.add_argument("-printFileName", type=bool, dest="printFileName", help="Print the file name with the category.", required=False, default=False)
 args = parser.parse_args()
 
 if not path.exists(args.filename):
     raise Exception("File '" + args.filename + "' not found.")
 
+printFileName = args.printFileName
+
 if path.isdir(args.filename):
-    processDir(args.filename)
+    processDir(args.filename, printFileName)
 elif path.isfile(args.filename):
-    processFile(args.filename)
+    processFile(args.filename, printFileName)
